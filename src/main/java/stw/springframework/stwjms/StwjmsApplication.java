ef652eb1baf61d598df6325508fa6b07d2d0decc
ScienceTechWorks <sciencetechworks@gmail.com>
2021-12-30 18:07:40 +0100

    Initial Project.

---
+++ src/main/java/stw/springframework/stwjms/StwjmsApplication.java
@@ -0,0 +1,13 @@
+package stw.springframework.stwjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StwjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StwjmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stw/springframework/stwjms/StwjmsApplicationTests.java
@@ -0,0 +1,13 @@
+package stw.springframework.stwjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StwjmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
