cdbf6a21a7c6fb39416c8b1669c514d39280e85b
ScienceTechWorks <sciencetechworks@gmail.com>
2021-12-30 18:36:14 +0100

    Launch JMS Server.

--- src/main/java/stw/springframework/stwjms/StwjmsApplication.java
+++ src/main/java/stw/springframework/stwjms/StwjmsApplication.java
@@ -1,12 +1,25 @@
 package stw.springframework.stwjms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StwjmsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+                // Launch ActiveMQ Server
+                ActiveMQServer server= ActiveMQServers.newActiveMQServer(
+                        new ConfigurationImpl()
+                                .setPersistenceEnabled(false)
+                                .setJournalDirectory("target/data/journal")
+                                .setSecurityEnabled(false)
+                                .addAcceptorConfiguration("invm","vm://0"));
+                
+                server.start();
+                
 		SpringApplication.run(StwjmsApplication.class, args);
 	}
 
